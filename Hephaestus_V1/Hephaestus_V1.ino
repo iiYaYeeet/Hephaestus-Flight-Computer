10c2fce7484e21597e07bc4e0fa91f06e2fb9774
MSI\CJRow <cjrowland05@gmail.com>
2026-04-02 11:14:32 -0400

    updated comments and updated docs

--- Hephaestus_V1/Hephaestus_V1.ino
+++ Hephaestus_V1/Hephaestus_V1.ino
@@ -2,51 +2,46 @@
 #include <Adafruit_BMP280.h>
 #include "MPU6050_6Axis_MotionApps20.h"
 
+/*
+* Hephaestus flight computer code
+* 1.0
+* Flight count - 0
+* Galalumga board
+* Altitude, Temperature, Pressure from BMP280 breakout board
+* Averaged gyro and acceleration from dual GY-251 MPU6050 breakout boards
+* Written by CR_DGD
+*/
+
+//Pins
 const int buzzer = 8;
 const int ledrxtx = 6;
 const int ledon = 9;
 const int goled = 13;
-//Found device at 0x68
-//Found device at 0x69
-//Found device at 0x76
 
-uint8_t FIFOBuffer[64]; // FIFO storage buffer
-VectorFloat gravity;    // [x, y, z]            Gravity vector
-float euler[3];         // [psi, theta, phi]    Euler angle container
-float ypr[3];           // [yaw, pitch, roll]   Yaw/Pitch/Roll container and gravity vector
+//Log time value
+int logstep = 0;
 
+//------------------------------------------- SENSOR DECLARE ---------------------------------------------------
 
 //MPU1
 MPU6050 mpu1(0x68, &Wire2);
-bool DMPReady1 = false;  // Set true if DMP init was successful
-uint8_t MPUIntStatus1;   // Holds actual interrupt status byte from MPU
-uint8_t devStatus1;      // Return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize1;    // Expected DMP packet size (default is 42 bytes)
-/*---Orientation/Motion Variables---*/ 
-Quaternion q1;           // [w, x, y, z]         Quaternion container
-VectorInt16 aa1;         // [x, y, z]            Accel sensor measurements
-VectorInt16 gy1;         // [x, y, z]            Gyro sensor measurements
-
+//Found device at 0x68
 
 //MPU2
 MPU6050 mpu2(0x69, &Wire2);
-bool DMPReady2 = false;  // Set true if DMP init was successful
-uint8_t MPUIntStatus2;   // Holds actual interrupt status byte from MPU
-uint8_t devStatus2;      // Return status after each device operation (0 = success, !0 = error)
-uint16_t packetSize2;    // Expected DMP packet size (default is 42 bytes)/*---Orientation/Motion Variables---*/ 
-Quaternion q2;           // [w, x, y, z]         Quaternion container
-VectorInt16 aa2;         // [x, y, z]            Accel sensor measurements
-VectorInt16 gy2;         // [x, y, z]            Gyro sensor measurements
+//Found device at 0x69
 
 #define CHIP_ID_REG 0xD0
 //BMP
+//Found device at 0x76
 Adafruit_BMP280 bmp(&Wire2);
 Adafruit_Sensor *bmp_temp = bmp.getTemperatureSensor();
 Adafruit_Sensor *bmp_pressure = bmp.getPressureSensor();
 
+
 void setup() 
 {
-
+//------------------------------------------- SERIAL AND PIN INIT -------------------------------------------
  //SERIAL & I2C
   Serial.begin(9600);
   Wire2.begin();
@@ -58,7 +53,8 @@ void setup()
   pinMode(goled, OUTPUT);
   Serial.println("Initializing pins");
 
-//INITALIZE BMP
+//------------------------------------------- SENSOR INIT ---------------------------------------------------
+//------------------------------------------- INITALIZE BMP -------------------------------------------------
   unsigned status;
   Serial.println("Initializing BMP");
   status = bmp.begin(0x76);
@@ -75,8 +71,17 @@ void setup()
                   Adafruit_BMP280::FILTER_X16,      /* Filtering. */
                   Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
 
+  digitalWrite(goled,HIGH);
+  delay(60);
+  digitalWrite(goled,LOW);
+  delay(60);
+  digitalWrite(goled,HIGH);
+  delay(60);
+  digitalWrite(goled,LOW);
+  delay(60);                
 
-//INITALIZE MPU1
+
+//------------------------------------------- INITALIZE MPU1 -------------------------------------------------
   Serial.println("Initializing MPU1");
   mpu1.initialize();
   mpu1.setFullScaleGyroRange(MPU6050_GYRO_FS_250);
@@ -96,7 +101,7 @@ void setup()
   digitalWrite(goled,LOW);
   delay(60);
 
-//INITALIZE MPU2
+//------------------------------------------- INITALIZE MPU2 -------------------------------------------------
   Serial.println("Initializing MPU2");
   mpu2.initialize();
   mpu2.setFullScaleGyroRange(MPU6050_GYRO_FS_250);
@@ -121,7 +126,8 @@ void setup()
   Wire2.write(CHIP_ID_REG);
   Wire2.endTransmission();
 
-//BMP Test
+
+//------------------------------------------- BMP TEST -------------------------------------------------
   Wire2.requestFrom(0x76, 1);
   if (Wire2.available()) {
     byte id = Wire2.read();
@@ -137,7 +143,9 @@ void setup()
     Serial.println("No response from BMP280.");
   }
 
-//MPU Test
+
+//------------------------------------------- MPU TEST -------------------------------------------------
+//MPU1
   if (!mpu1.testConnection()) 
   {
     Serial.println("MPU1 Connection Failed");
@@ -146,6 +154,7 @@ void setup()
   {
     Serial.println("MPU1 Online");
   }
+//MPU2
   if (!mpu2.testConnection()) 
   {
     Serial.println("MPU2 Connection Failed");
@@ -154,10 +163,25 @@ void setup()
   {
     Serial.println("MPU2 Online");
   }
+
+  digitalWrite(goled,HIGH);
+  delay(120);
+  digitalWrite(goled,LOW);
+  delay(120);
+  digitalWrite(goled,HIGH);
+  delay(120);
+  digitalWrite(goled,LOW);
+  delay(120);
+  digitalWrite(goled,HIGH);
+  delay(240);
+  digitalWrite(goled,LOW);
+  Serial.println("System check complete. Logging started");
 }
 
 void loop()
 {
+  logstep++;
+//------------------------------------------- MPU LOG -------------------------------------------------
   int16_t ax1, ay1, az1, gx1, gy1, gz1; //MPU1
   mpu1.getMotion6(&ax1, &ay1, &az1, &gx1, &gy1, &gz1);
   int16_t ax2, ay2, az2, gx2, gy2, gz2; //MPU2
@@ -171,13 +195,14 @@ void loop()
   avg_gy = avg(gy1,gy2);
   avg_gz = avg(gz1,gz2);
 
+//------------------------------------------- BMP LOG -------------------------------------------------
   float f = bmp.readTemperature();
   float P = bmp.readPressure()/100;
   float A = bmp.readAltitude(1013);
 
 
-  //Serial.print("AccelX: "); 
-  formatpacket(1,f,A,P,avg_ax/16384.0,avg_ay/16384.0,avg_az/16384.0,avg_gx/131,avg_gy/131,avg_gz/131);
+//------------------------------------------- FORMAT LOG -------------------------------------------------
+  formatpacket(logstep,f,A,P,avg_ax/16384.0,avg_ay/16384.0,avg_az/16384.0,avg_gx/131,avg_gy/131,avg_gz/131);
   delay(50);
 }
 
